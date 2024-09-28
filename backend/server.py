3534fd964c7d611ec9de22e0b73d7840639cdabc
Dylan Toth <dylant1@yahoo.com>
2024-09-28 16:57:16 -0400

    Add mongodb compatibility

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,21 @@ from flask import Flask, request
 from utils import get_youtube_subtitles
 from dotenv import load_dotenv
 from call_ai import call_ai
+import os
+import datetime
+from pymongo.mongo_client import MongoClient
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './files'
 load_dotenv()
+
+
+uri = os.getenv("MONGODB_URI")
+mongo_client = MongoClient(uri) 
+db = mongo_client['check-mate']  
+collection = db['recent-videos']
+
 
 @app.route('/', methods=['GET'])
 def index():
@@ -16,6 +26,12 @@ def index():
 def factcheck():
     text = request.json['url']
     if "youtube" in text:
+        # add to database
+        document = {
+            "url": text,
+            "timestamp": datetime.datetime.now()  
+        }
+        collection.insert_one(document)
         transcript = get_youtube_subtitles(text)
         return call_ai(transcript)
     else:
