8890f0b1864f07995e05320ac47935dc5d98b1ba
Areeb <islamareeb.21@gmail.com>
2024-09-28 13:41:11 -0400

    gpt wrapper1

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,21 @@
 from flask import Flask
-
+from dotenv import load_dotenv
+from call_ai import call_ai
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './files'
+load_dotenv()
 
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello World'
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    text = request.form['text']
+    return call_ai(text)
+           
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
