021b21440b9383250826ba497d1ce75b3ae417ea
Kody Takada <kodytakada@icloud.com>
2024-09-28 14:40:54 -0400

    Added sidebar

---
+++ extension/background.js
@@ -0,0 +1,12 @@
+chrome.action.onClicked.addListener((tab) => {
+  console.log("Onclick Running");
+
+  // Ensure the extension runs only on YouTube tabs
+  if (tab.url.includes("youtube.com")) {
+    console.log("YouTube URL recognized");
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['./scripts/content.js']
+    });
+  }
+});
