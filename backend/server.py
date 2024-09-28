79f804cb79c0586e69d5dd2aa19e7580f9fc6b72
Dylan Toth <dylant1@yahoo.com>
2024-09-28 13:51:52 -0400

    Add transcript function with condensed version

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,14 @@
 from flask import Flask
-
-
+from utils import get_youtube_subtitles
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './files'
 
-@app.route('/', methods=['GET']):
+@app.route('/', methods=['GET'])
 def index():
-    return 'Hello World'
+    return get_youtube_subtitles("https://www.youtube.com/watch?v=4QXUdPskzNs&ab_channel=LiveNOWfromFOX")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+
+
